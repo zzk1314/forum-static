7397cb513d23d754f8992deb1a10b1640659f2a2
nethunder <17089113140@163.com>
2017-03-24 14:56:58 +0800

    fix del

--- src/modules/fragment/challenge/DoChallenge.tsx
+++ src/modules/fragment/challenge/DoChallenge.tsx
@@ -9,7 +9,6 @@ import FlatButton from 'material-ui/FlatButton';
 import Snackbar from 'material-ui/Snackbar';
 import {loadSelfChallengeSubmit,submitChallenge} from "./async"
 import VerticalBarLoading from "../../../components/VerticalBarLoading"
-import Editor from "../../../components/editor/Editor"
 
 @connect(state => state)
 export default class DoChallenge extends React.Component<any,any> {
@@ -89,8 +88,7 @@ export default class DoChallenge extends React.Component<any,any> {
     const {challenge,location} = this.props;
     const {challengeId, planId} = location.query;
     const mine = _.get(challenge,`mine.${planId}.${challengeId}`,{});
-    const {submitId} = mine;
-    var content = this.refs.editor.getValue()
+    const {content, submitId} = mine;
     if (_.isEmpty(content)) {
       this.showAlert("作业还没写完哦","提示");
       return;
@@ -146,8 +144,9 @@ export default class DoChallenge extends React.Component<any,any> {
             目标最好是某个具体问题或场景 <br/>
             制定目标之前，可以先回顾该专题的知识体系<br/>
           </div>
-          <Editor ref="editor" value={content} moduleId="2">
-          </Editor>
+          <textarea cols="30" rows="10"
+                    value={content}
+                    onChange={(e) => dispatch(set(`challenge.mine.${planId}.${challengeId}.content`,e.currentTarget.value))}/>
           <div className="submitBtnGroup">
             <PicUpload onUploadSuccess={(url)=>this.onUploadSuccess(url)} moduleId={moduleId}
                        referencedId={submitId}/>
