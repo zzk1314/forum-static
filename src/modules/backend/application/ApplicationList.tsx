aa85e89c8d48c36edfeb92ae867c24aa95edab41
三十文 <xfduan@duanxianfengdeMacBook-Pro.local>
2017-05-24 16:26:01 +0800

    pc app comment

--- src/modules/backend/application/ApplicationList.tsx
+++ src/modules/backend/application/ApplicationList.tsx
@@ -11,13 +11,13 @@ import "./ApplicationList.less"
 import {loadApplicationSubmit, highlight, loadApplication, submitComment} from  "./async"
 
 export const CommentType = {
-  Challenge:1,
-  Application:2,
-  Subject:3,
+  Challenge: 1,
+  Application: 2,
+  Subject: 3,
 }
 
 @connect(state => state)
-export default class ApplicationList extends React.Component<any,any> {
+export default class ApplicationList extends React.Component<any, any> {
   static contextTypes = {
     router: React.PropTypes.object.isRequired
   }
@@ -26,21 +26,21 @@ export default class ApplicationList extends React.Component<any,any> {
     super(props);
     this.state = {
       other: [],
-      hasMore:true,
+      hasMore: true,
       index: 1,
-      otherLoading:true,
-      application:{},
+      otherLoading: true,
+      application: {},
     }
   }
 
 
   componentWillMount() {
     const {location, dispatch, page} = this.props;
-    const {applicationId} =  location.query
-    const {index} =  this.state
-    const scrollValue = _.get(page,"scroll");
+    const {applicationId} = location.query
+    const {index} = this.state
+    const scrollValue = _.get(page, "scroll");
 
-    loadApplication(applicationId).then(res =>{
+    loadApplication(applicationId).then(res => {
       if (res.code === 200) {
         this.setState({application: res.msg});
       } else {
@@ -55,9 +55,9 @@ export default class ApplicationList extends React.Component<any,any> {
     loadApplicationSubmit(applicationId, index).then(res => {
       if (res.code === 200) {
         this.setState({other: res.msg, otherLoading: false});
-        if(scrollValue){
-          scroll(scrollValue.x,scrollValue.y);
-          dispatch(set("page.scroll",{x:0,y:0}));
+        if (scrollValue) {
+          scroll(scrollValue.x, scrollValue.y);
+          dispatch(set("page.scroll", {x: 0, y: 0}));
         }
       } else {
         this.setState({otherLoading: false});
@@ -73,26 +73,26 @@ export default class ApplicationList extends React.Component<any,any> {
 
   showAlert(content, title) {
     const {dispatch} = this.props;
-    dispatch(alertMsg(title,content));
+    dispatch(alertMsg(title, content));
     setTimeout(() => {
-      dispatch(set("base.showModal",false));
+      dispatch(set("base.showModal", false));
     }, 1000);
   }
 
-  loadMoreContent(){
+  loadMoreContent() {
     const {location, dispatch} = this.props;
-    const {applicationId} =  location.query
-    const {index, other} =  this.state
+    const {applicationId} = location.query
+    const {index, other} = this.state
 
-    loadApplicationSubmit(applicationId, index+1).then(res => {
+    loadApplicationSubmit(applicationId, index + 1).then(res => {
       if (res.code === 200) {
         let hasMore = true
-        if(res.msg.length===0){
+        if (res.msg.length === 0) {
           hasMore = false
-        }else {
+        } else {
           res.msg.forEach(item => other.push(item));
         }
-        this.setState({other, index:index+1, hasMore});
+        this.setState({other, index: index + 1, hasMore});
       } else {
         throw new BreakSignal(res.msg, "提示");
       }
@@ -103,14 +103,14 @@ export default class ApplicationList extends React.Component<any,any> {
     })
   }
 
-  highlight(application, id){
+  highlight(application, id) {
     const {other} = this.state
-    highlight(application, id).then(res =>{
+    highlight(application, id).then(res => {
       if (res.code === 200) {
         this.showAlert('提交成功')
       }
-      other.forEach((item)=>{
-        if(item.id === id){
+      other.forEach((item) => {
+        if (item.id === id) {
           _.set(item, 'priority', 1)
         }
       })
@@ -118,11 +118,11 @@ export default class ApplicationList extends React.Component<any,any> {
     })
   }
 
-  showComment(id){
+  showComment(id) {
     const {other} = this.state
 
-    other.forEach((item)=>{
-      if(item.id === id){
+    other.forEach((item) => {
+      if (item.id === id) {
         _.set(item, 'commenting', 1)
       }
     })
@@ -130,19 +130,30 @@ export default class ApplicationList extends React.Component<any,any> {
     this.setState({other})
   }
 
-  comment(id){
+  comment(id) {
     const {other, comment} = this.state
-    submitComment(CommentType.Application, id, comment).then(res =>{
+    submitComment(CommentType.Application, id, comment).then(res => {
       if (res.code === 200) {
         this.showAlert('提交成功')
       }
-      other.forEach((item)=>{
-        if(item.id === id){
+      other.forEach((item) => {
+        if (item.id === id) {
           _.set(item, 'comment', 1)
           _.set(item, 'commenting', 0)
         }
       })
-      this.setState({other, comment:''})
+      this.setState({other, comment: ''})
+    })
+  }
+
+  onClickGoDetail(item) {
+    this.context.router.push({
+      pathname: "/fragment/application/show",
+      query: {
+        submitId: item.id,
+        applicationId: item.applicationId,
+        planId: item.planId
+      }
     })
   }
 
@@ -154,48 +165,45 @@ export default class ApplicationList extends React.Component<any,any> {
           {other.map((item, index) => {
             const {id, upName, headPic, upTime, content, applicationId, priority, comment, commenting} = item;
             return (
-                <div key={index} className="workItemContainer" style={{marginTop:50}}>
-                  <div className="titleArea">
-                    <div className="leftArea">
-                      <div className="author">
-                        <div className="avatar">
-                          <Avatar
-                              src={headPic}
-                              size={30}
-                          />
-                        </div>
-                        <div className="upInfo">
-                          <div className="upName">{upName}</div>
-                          <div className="upTime">{upTime + "上传"}</div>
-                        </div>
+              <div key={index} className="workItemContainer" style={{marginTop: 50}}>
+                <div className="titleArea">
+                  <div className="leftArea">
+                    <div className="author">
+                      <div className="avatar">
+                        <Avatar
+                          src={headPic}
+                          size={30}
+                        />
+                      </div>
+                      <div className="upInfo">
+                        <div className="upName">{upName}</div>
+                        <div className="upTime">{upTime + "上传"}</div>
                       </div>
                     </div>
                   </div>
-                  <div className="workContentContainer">
-                    <div className="content" dangerouslySetInnerHTML={{__html:content}}>
-                    </div>
-                    <div className="rightArea">
-                      {priority === 0 ?
-                        <div className="function-button" onClick={()=>this.highlight(applicationId, id)}>
-                          加精
-                        </div>:
-                      <div className="function-done">已加精</div> }
-                      {comment === 0 ?
-                          <div className="function-button" onClick={()=>this.showComment(id)}>
-                            评论
-                          </div>:
-                          <div className="function-done">已评论</div> }
-
-                    </div>
-                    {commenting === 1?
-                        <div className="commentSubmit">
-                                <textarea value={this.state.comment} placeholder="和作者切磋讨论一下吧"
-                                          onChange={(e)=>{this.setState({comment:e.target.value})}}/>
-                          <div className="commentBtn" onClick={()=>this.comment(id)}>提交</div>
-                        </div>:null
-                    }
-                  </div>
                 </div>
+                <div className="workContentContainer">
+                  <div className="content" dangerouslySetInnerHTML={{__html: content}}>
+                  </div>
+                  <div className="rightArea">
+                    {priority === 0 ?
+                      <div className="function-button" onClick={() => this.highlight(applicationId, id)}>
+                        加精
+                      </div> :
+                      <div className="function-done">已加精</div> }
+                      <div className="function-button" onClick={() => this.onClickGoDetail(item)}>详情</div>
+                  </div>
+                  {commenting === 1 ?
+                    <div className="commentSubmit">
+                                <textarea value={this.state.comment} placeholder="和作者切磋讨论一下吧"
+                                          onChange={(e) => {
+                                            this.setState({comment: e.target.value})
+                                          }}/>
+                      <div className="commentBtn" onClick={() => this.comment(id)}>提交</div>
+                    </div> : null
+                  }
+                </div>
+              </div>
             )
           })}
         </div>
@@ -204,14 +212,14 @@ export default class ApplicationList extends React.Component<any,any> {
     return (
       <div className="applicationListContainer">
         <div className="myApplicationContainer">
-          <div className="desc" dangerouslySetInnerHTML={{__html:application.description}}></div>
+          <div className="desc" dangerouslySetInnerHTML={{__html: application.description}}></div>
           <div className="title">
             <span className="title-text">群众的智慧</span>
           </div>
-          {otherLoading ?<VerticalBarLoading/>: renderOther()}
+          {otherLoading ? <VerticalBarLoading/> : renderOther()}
           <Divider/>
-          {hasMore ?<div className="more" onClick={()=>this.loadMoreContent()}>点击加载更多</div>:
-              <div className="no-more">没有更多了</div>}
+          {hasMore ? <div className="more" onClick={() => this.loadMoreContent()}>点击加载更多</div> :
+            <div className="no-more">没有更多了</div>}
         </div>
       </div>
     )
