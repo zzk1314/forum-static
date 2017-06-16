2885b6d5f40bda3ac0442632b849c6e45b6c03ab
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-06-16 15:43:55 +0800

    update

--- src/modules/fragment/problem/ProblemViewer.tsx
+++ src/modules/fragment/problem/ProblemViewer.tsx
@@ -151,7 +151,7 @@ export default class ProblemViewer extends React.Component<any, any> {
               <div className="context-title-img">
                 <AssetImg width={'60%'} url={categoryPic} marginTop="25"/>
               </div>
-              <div className="text2"
+              <div className="text click-key"
                    onClick={()=>window.location.href='https://mp.weixin.qq.com/s?__biz=MzA5ODI5NTI5OQ==&mid=2651673801&idx=1&sn=c0bc7ad463474f5d8f044ae94d8e6af7&chksm=8b6a3fa5bc1db6b335c423b51e8e987c0ba58546c9a4bcdba1c6ea113e710440e099981fac22&mpshare=1&scene=1&srcid=0522JbB9FCiJ2MLTYIJ9gHp8&key=97c2683b72ba12a9fe14a4718d1e2fc1db167b4659eda45c59be3b3c39723728975cf9c120462d5d896228edb74171fb9bfefc54a6ff447b7b3389e626e18744f9dca6103f6a3fbeb523c571631621eb&ascene=0&uin=MjYxMjUxOTM4MA%3D%3D&devicetype=iMac+MacBookPro11%2C1+OSX+OSX+10.10.5+build(14F27)&version=12010310&nettype=WIFI&fontScale=100&pass_ticket=sl95nanknHuEvflHY9fNI6KUKRA3koznfByp5C1nOV70kROWRuZNqQwkqvViYXiw'}
               >了解"个人势能模型"</div>
 
