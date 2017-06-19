bb81a2d30828ed0dbaab278f77114239aa7f56bc
nethunder <17089113140@163.com>
2017-06-19 14:51:34 +0800

    fix

--- src/modules/backend/operation/async.ts
+++ src/modules/backend/operation/async.ts
@@ -1,7 +1,5 @@
 import {pget,ppost} from "../../../utils/request"
 
 export function sendMessage(phone,content){
-  let phones = [];
-  phones.push(phone);
-  return ppost("/operation/send/message",{content:content,phones:phones})
+  return ppost("/operation/send/message",{content:content,phone:phone})
 }
