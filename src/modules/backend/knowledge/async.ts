310015f388ef20cf5e0c2600f6b42f4435837d90
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-09-16 09:08:40 +0800

    change action router

---
+++ src/modules/backend/knowledge/async.ts
@@ -0,0 +1,21 @@
+import { pget, ppost } from '../../../utils/request'
+
+export function loadEditableProblem() {
+  return pget(`/pc/operation/knowledge/get/problem`)
+}
+
+export function loadKnowledgeDetail(knowledgeId) {
+  return pget(`/pc/operation/knowledge/get/knowledge/${knowledgeId}`)
+}
+
+export function addNewChapter(chapter) {
+  return ppost(`/pc/operation/knowledge/post/add/chapter/${chapter}`)
+}
+
+export function addNewSection(chapter, section) {
+  return ppost(`/pc/operation/knowledge/post/add/section/${chapter}/${section}`)
+}
+
+export function updateKnowledge(param) {
+  return ppost(`/pc/operation/knowledge//post/update/knowledge`, param)
+}
