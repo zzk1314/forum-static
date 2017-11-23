764bada80a350905fefd004427f218246b5535b7
richardyangcz <richardyangcz@sina.com>
2017-11-23 10:16:43 +0600

    delete *.jsx

--- src/modules/backend/import/application/async.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var request_1 = require("utils/request");
-// 获取所有小课和对应知识点集合的列表
-function loadAllProblemsAndKnowledges() {
-    return request_1.pget("/pc/operation/warmup/load/knowledges");
-}
-exports.loadAllProblemsAndKnowledges = loadAllProblemsAndKnowledges;
