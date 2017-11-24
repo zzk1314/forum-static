2bed1b75f1d98fb92cb1a74afa62f04623ba2294
richardyangcz <richardyangcz@sina.com>
2017-11-24 11:13:36 +0600

    add update AppliactionPractice page

--- src/modules/backend/import/application/async.ts
+++ src/modules/backend/import/application/async.ts
@@ -9,3 +9,18 @@ export function loadAllProblemsAndKnowledges() {
 export function insertApplicationPractice (param){
   return ppost(`/pc/operation/application/insert/practice`, param)
 }
+
+//加载某道应用题
+export function loadApplication(applicationId){
+  return pget(`/pc/fragment/application/load/${applicationId}`);
+}
+
+//更新某道应用题
+export function updateApplicationPractice(applicationId, topic, description,difficulty) {
+  return ppost(`/pc/fragment/application/update/${applicationId}`, {topic: topic, description: description,difficulty:difficulty})
+}
+
+//获取所有知识点
+export function loadAllKnowledges(){
+  return pget('/pc/operation/knowledge/query/knowledges')
+}
