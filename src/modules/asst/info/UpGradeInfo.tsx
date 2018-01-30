ac7e69865f278762e8817f64cf0b86343914ed15
richardyangcz <richardyangcz@sina.com>
2018-01-30 21:01:46 +0600

    fix bug

--- src/modules/asst/info/UpGradeInfo.tsx
+++ src/modules/asst/info/UpGradeInfo.tsx
@@ -243,7 +243,7 @@ export default class UpGradeInfo extends React.Component<any, any> {
         <div>{`您完成的培养新人次数为：${data.fosterNew}，还需要培养新人的次数为：${data.remainFosterNew}`}</div>} {data.needCompanyNumber !==
         0 && <div>{`您完成的企业培训次数为：${data.companyNumber}，还需要完成的企业培训次数为：${data.remainCompanyNumber}`}</div>}
           {data.needCompanyScore !== 0 &&
-          <div>{`您需要的企业培训的分数为：${data.needCompanyScore}，目前的分数为：${data.companyScore}`}</div>} </div>
+          <div>{`您需要的企业培训的分数为：${data.needCompanyScore}，目前的分数为：${data.companyTrainScore}`}</div>} </div>
       )
     }
 
