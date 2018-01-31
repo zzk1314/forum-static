502b4ff234eb8da6f339504dfd22f622ec99a694
xfduan <1047210087@qq.com>
2018-01-31 21:15:41 +0800

    fix request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -17,7 +17,7 @@ axios.interceptors.response.use(function(response) {
 })
 
 function pget(url: string, query?: Object) {
-  return get(`${url}${appendQs(query)}`, {
+  return get(`${url}${_appendQs(query)}`, {
     validateStatus: function(status) {
       return status >= 200 && status < 300 || status == 700
     }
@@ -43,15 +43,15 @@ function ppost(url: string, body: Object) {
 }
 
 function log(msg, url) {
-  return post('/b/log', JSON.stringify({result: msg, cookie: document.cookie, url: url}))
-}
-
-function appendQs(query: Object): string {
-  return !query ? '' : `?${qs.stringify(query)}`
+  return post('/b/log', JSON.stringify({ result: msg, cookie: document.cookie, url: url }))
 }
 
 function mark(param) {
   return ppost('/rise/b/mark', param)
+}
+
+function _appendQs(query: Object): string {
+  return !query ? '' : `?${qs.stringify(query)}`
 }
 
 export class BreakSignal {
