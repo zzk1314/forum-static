73832f6d265865d8cb7a400afcad24e65fbdb897
richardyangcz <richardyangcz@sina.com>
2018-02-24 15:31:14 +0600

    modify choice

--- src/modules/backend/import/warmup/WarmupShowList.tsx
+++ src/modules/backend/import/warmup/WarmupShowList.tsx
@@ -1,13 +1,12 @@
-import * as React from "react";
-import {connect} from "react-redux";
-import "./WarmupShowList.less"
-import {set, startLoad, endLoad, alertMsg} from "../../../../redux/actions"
-import {loadWarmupList} from "./async"
-import {BreakSignal, Stop} from "../../../../utils/request"
+import * as React from 'react'
+import { connect } from 'react-redux'
+import './WarmupShowList.less'
+import { set, startLoad, endLoad, alertMsg } from '../../../../redux/actions'
+import { loadWarmupList } from './async'
+import { BreakSignal, Stop } from '../../../../utils/request'
 import Divider from 'material-ui/Divider'
 import Subheader from 'material-ui/Subheader'
-import {removeHtmlTags} from "../../../../utils/textUtils"
-
+import { removeHtmlTags } from '../../../../utils/textUtils'
 
 @connect(state => state)
 export default class WarmupShowList extends React.Component<any, any> {
@@ -17,51 +16,48 @@ export default class WarmupShowList extends React.Component<any, any> {
   }
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      practiceList: [],
+      practiceList: []
     }
   }
 
   componentWillMount(problemId) {
-    if (!problemId) {
+    if(!problemId) {
       problemId = this.props.location.query.problemId
     }
     loadWarmupList(problemId).then(res => {
-      if (res.code === 200) {
+      if(res.code === 200) {
         this.setState({
           practiceList: res.msg
         })
-      } else if (res.code === 401) {
+      } else if(res.code === 401) {
         this.context.router.push({
-          pathname: "/login",
+          pathname: '/login',
           query: {
             callbackUrl: `/backend/warmup/show/list?problemId=${problemId}`
           }
         })
-      } else if (res.code === 403) {
-        setTimeout(() => window.location.href = "/403.jsp", 500);
+      } else if(res.code === 403) {
+        setTimeout(() => window.location.href = '/403.jsp', 500)
       } else {
-        throw new BreakSignal(res.msg, "加载当前问题失败")
+        throw new BreakSignal(res.msg, '加载当前问题失败')
       }
     })
   }
 
   componentWillReceiveProps(newProps) {
-    if (this.props.location.query.problemId !== newProps.location.query.problemId) {
+    if(this.props.location.query.problemId !== newProps.location.query.problemId) {
       this.componentWillMount(newProps.location.query.problemId)
     }
   }
 
   view(practice) {
-    this.context.router.push({
-      pathname: '/backend/warmup/view',
-      query: {id: practice.id}
-    })
+    window.open(`/backend/warmup/view?id=${practice.id}`)
   }
 
   render() {
-    const {practiceList = []} = this.state
+    const { practiceList = [] } = this.state
 
     const renderPractice = (practiceList) => {
       return (
@@ -71,6 +67,9 @@ export default class WarmupShowList extends React.Component<any, any> {
               <div className="practice" onClick={() => {
                 this.view(practice)
               }}>
+                {practice.example ? `【第${practice.chapter}章第${practice.section}节例题】` :
+                  `【第${practice.chapter}章第${practice.section}节第${practice.sequence}道】`
+                }
                 {
                   removeHtmlTags(practice.question).length > 40 ?
                     removeHtmlTags(practice.question).substring(0, 40).concat(' ...') : removeHtmlTags(practice.question)
