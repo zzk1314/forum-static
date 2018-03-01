e213078f15f4ccbad1ed4e2c5bc2f6716657b82c
richardyangcz <richardyangcz@sina.com>
2018-03-01 11:06:03 +0600

    add user info

--- src/modules/backend/user/UserInfo.tsx
+++ src/modules/backend/user/UserInfo.tsx
@@ -3,10 +3,18 @@ import { connect } from 'react-redux'
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
 import { startLoad, endLoad, alertMsg } from '../../../redux/actions'
-import { searchInfoById, searchInfoByMember, searchInfoByRiseId } from './async'
+import { searchInfo, searchInfoByClass } from './async'
 import Dialog from 'material-ui/Dialog'
-import { Divider } from 'material-ui'
+import { Divider, MenuItem, SelectField } from 'material-ui'
 import './UserInfo.less'
+import { loadClassNameAndGroup } from '../../asst/async'
+import * as _ from 'lodash'
+import { MessageTable } from '../message/autoreply/MessageTable'
+
+const cellStyle = {
+  paddingLeft: 0,
+  paddingRight: 0
+}
 
 @connect(state => state)
 export default class UserInfo extends React.Component<any, any> {
@@ -14,31 +22,52 @@ export default class UserInfo extends React.Component<any, any> {
   constructor() {
     super()
     this.state = {
-      memberId: '',
-      profileId: '',
-      riseId: '',
+      meta: [
+        { tag: 'nickname', alias: '昵称', style: _.merge({}, cellStyle, { width: '100px' }) },
+        { tag: 'memberId', alias: '学号', style: _.merge({}, cellStyle, { width: '100px' }) },
+        { tag: 'memberType', alias: '会员类型', style: _.merge({}, cellStyle, { width: '100px' }) },
+        { tag: 'openDate', alias: '入学日期', style: _.merge({}, cellStyle, { width: '100px' }) }
+      ],
+      searchId: '',
+      selected: false,
+      classSearch: false,
+      className: '',
+      groupId: '',
+      classNames: [],
+      groupIds: [],
       openDialog: false,
+      infos: [],
       info: ''
     }
+  }
+
+  componentWillMount() {
+    loadClassNameAndGroup().then(res => {
+      const { code, msg } = res
+      if(code === 200) {
+        this.setState({
+          classNames: msg.className,
+          groupIds: msg.groupIds
+        })
+      }
+    })
   }
 
   /**
    * 根据学号查询
    * @param memberId
    */
-  searchInfoByMemberId(memberId) {
+  searchUserInfo(searchId) {
     const { dispatch } = this.props
     dispatch(startLoad())
 
-    searchInfoByMember(memberId).then(res => {
+    searchInfo(searchId).then(res => {
       dispatch(endLoad())
       const { code, msg } = res
       if(code === 200) {
         this.setState({
-          openDialog: true,
-          info: msg
+          infos: msg
         })
-
       }
       else {
         dispatch(alertMsg(msg))
@@ -46,97 +75,145 @@ export default class UserInfo extends React.Component<any, any> {
     })
   }
 
-  /**
-   * 根据profileId查询
-   * @param id
-   */
-  searchInfoById(id){
-    const {dispatch} = this.props
-    if(isNaN(id)){
-      dispatch(alertMsg('请输入数字'))
+  searchClass = () => {
+    const { dispatch } = this.props
+    const { className, groupId } = this.state
+    if(_.isEmpty(className) || _.isEmpty(groupId)) {
+      dispatch(alertMsg('请将班级和小组选择完整'))
       return
     }
-
     dispatch(startLoad())
-
-    searchInfoById(id).then(res=>{
+    searchInfoByClass(className, groupId).then(res => {
       dispatch(endLoad())
-      const{code,msg} = res
-      if(code === 200){
+      const { code, msg } = res
+      if(code === 200) {
         this.setState({
-          openDialog:true,
-          info:msg
+          infos: msg
         })
-      }
-      else{
+      } else {
         dispatch(alertMsg(msg))
       }
     })
+
   }
 
-  /**
-   * 根据RiseId查询
-   * @param riseId
-   */
-  searchInfoByRiseId(riseId){
-    const{dispatch} = this.props
-    dispatch(startLoad())
-
-    searchInfoByRiseId(riseId).then(res=>{
-      dispatch(endLoad())
-      const{code,msg} = res
-      if(code===200){
-        this.setState({
-          openDialog:true,
-          info:msg
-        })
-      }
-      else{
-        dispatch(alertMsg(msg))
-      }
+  openDialog(item) {
+    this.setState({
+      openDialog: true,
+      info: item
     })
-
   }
 
   closeDialog = () => {
     this.setState({
-      openDialog: false,
-      memberId: '',
-      profileId: '',
-      riseId: '',
-      info: ''
+      openDialog: false
     })
   }
 
   render() {
-    const { memberId,profileId,riseId } = this.state
+    const { searchId, selected, classSearch } = this.state
+
+    const renderSelect = () => {
+
+      return (
+        <div className="select-container">
+          <RaisedButton
+            label="个人搜索" primary={true}
+            style={{ marginRight: 50 }}
+            onClick={() => this.setState({ classSearch: false, selected: true })}
+          />
+          <RaisedButton
+            label="班级和小组搜索" primary={true}
+            onClick={() => this.setState({
+              selected: true,
+              classSearch: true
+            })}
+          />
+        </div>
+      )
+    }
+
     const renderSearch = () => {
       return (
         <div className="search-container">
-          <div className="search-item-container">
-            <TextField hintText={'输入学号查询'} style={{ margin: 40,width:180}} value={memberId}
-                       onChange={(e, v) => this.setState({ memberId: v })}/>
-            <RaisedButton
-              label="点击查询" onClick={() => this.searchInfoByMemberId(memberId)}
-            /><br/>
-          </div>
-          <div className="search-item-container">
-            <TextField hintText={'输入profileId查询'} style={{ margin: 40,width:180 }} value={profileId}
-                       onChange={(e, v) => this.setState({ profileId: v })}/>
-            <RaisedButton
-              label="点击查询" onClick={() => this.searchInfoById(profileId)}
-            /><br/>
-          </div>
-          <div className="search-item-container">
-            <TextField hintText={'输入RiseId查询'} style={{ margin: 40,width:180 }} value={riseId}
-                       onChange={(e, v) => this.setState({ riseId: v })}/>
-            <RaisedButton
-              label="点击查询" onClick={() => this.searchInfoByRiseId(riseId)}
-            /><br/>
-          </div>
+          {classSearch ? renderClassSearch() : renderNormalSearch()}
         </div>
+      )
+    }
 
+    const renderNormalSearch = () => {
+      return (
+        <div className="search-item-container">
+          <TextField hintText={'输入学号/profileId/RiseId/昵称查询'} style={{ margin: 40, width: 300 }} value={searchId}
+                     onChange={(e, v) => this.setState({ searchId: v })}/>
+          <RaisedButton
+            label="点击查询" onClick={() => this.searchUserInfo(searchId)}
+          /><br/>
+          {renderMessage()}
+          {renderDialog()}
+        </div>
+      )
+    }
 
+    const renderClassSearch = () => {
+      return (
+        <div className="class-item-container">
+          {renderClassName()}
+          {renderGroupId()}
+          <div className="class-search-container">
+            <RaisedButton
+              label="点击查询" onClick={() => this.searchClass()}
+            /><br/>
+          </div>
+          {renderMessage()}
+          {renderDialog()}
+        </div>
+      )
+    }
+
+    const renderClassName = () => {
+      const { className, classNames } = this.state
+      return (
+        <div className="classname-container">
+          <SelectField
+            floatingLabelText="选择班级名" maxHeight={300} value={className} onChange={(ev, value) => {
+            this.setState({
+              className: ev.target.textContent,
+              groupId: ''
+            })
+          }}>
+            {
+              classNames.map((item, idx) => {
+                return (
+                  <MenuItem key={idx} value={item} primaryText={item}/>
+                )
+              })
+            }
+          </SelectField>
+        </div>
+      )
+    }
+
+    const renderGroupId = () => {
+      const { className, groupId, groupIds } = this.state
+      const group = _.filter(groupIds, { className: className })
+      return (
+        <div className="group-container">
+          <SelectField
+            floatingLabelText="选择小组" maxHeight={300} value={groupId} onChange={(ev, value) => {
+            this.setState({
+              groupId: ev.target.textContent
+            })
+          }}>
+            {
+              group.map((item, idx) => {
+                return (
+                  <MenuItem key={idx} value={item.groupId} primaryText={item.groupId}/>
+                )
+              })
+            }
+          </SelectField>
+        </div>
       )
     }
 
@@ -145,7 +222,7 @@ export default class UserInfo extends React.Component<any, any> {
       return (
         <Dialog open={openDialog} autoScrollBodyContent={true} modal={false}>
           <div className="bs-dialog">
-            <div className="bs-dialog-header" style={{ marginTop: '0px',marginBottom:'20px' }}>
+            <div className="bs-dialog-header" style={{ marginTop: '0px', marginBottom: '20px' }}>
               用户信息：
             </div>
             {renderDialogItem('昵称：', info.nickname)}
@@ -157,7 +234,7 @@ export default class UserInfo extends React.Component<any, any> {
             {renderDialogItem('联系方式：', info.mobileNo)}
             {renderDialogItem('婚恋情况：', info.married)}
 
-            <div className="bs-dialog-header" style={{ marginTop: '20px',marginBottom:'20px'  }}>
+            <div className="bs-dialog-header" style={{ marginTop: '20px', marginBottom: '20px' }}>
               会员信息
             </div>
             {renderDialogItem('当前会员类型：', info.memberType)}
@@ -165,7 +242,7 @@ export default class UserInfo extends React.Component<any, any> {
             {renderDialogItem('入学日期：', info.openDate)}
 
 
-            <div className="bs-dialog-header" style={{ marginTop: '20px',marginBottom:'20px'  }}>
+            <div className="bs-dialog-header" style={{ marginTop: '20px', marginBottom: '20px' }}>
               地址信息：
             </div>
             {renderDialogItem('国家：', info.country)}
@@ -192,19 +269,28 @@ export default class UserInfo extends React.Component<any, any> {
         <div className="bs-dialog-row" key={key}>
           <span className="bs-dialog-label">{label}</span>{br ? <br/> : null}
 
-          <span className={`${(value == '是' && label == '是否黑名单用户：') ? 'bs-dialog-true-value' : 'bs-dialog-value'}`}>
+          <span className='bs-dialog-value'>
             {value}
           </span>
-
           <Divider/>
         </div>
       )
     }
 
+    const renderMessage = () => {
+      return (
+        <MessageTable data={this.state.infos} meta={this.state.meta}
+                      opsButtons={[{
+                        editFunc: (item) => {this.openDialog(item)},
+                        opsName: '查看详情'
+                      }]}
+        />
+      )
+    }
+
     return (
       <div className="user-info-container">
-        {renderSearch()}
-        {renderDialog()}
+        {selected ? renderSearch() : renderSelect()}
       </div>
     )
   }
--- src/modules/backend/user/async.ts
+++ src/modules/backend/user/async.ts
@@ -1,15 +1,10 @@
-
 import { pget } from '../../../utils/request'
 
-export function searchInfoByMember(memberId){
-  return pget(`/pc/operation/user/search?type=1&&search=${memberId}`)
+export function searchInfo(searchId) {
+  return pget(`/pc/operation/user/search?searchId=${searchId}`)
 }
 
-export function searchInfoById(id){
-  return pget(`/pc/operation/user/search?type=2&&search=${id}`)
+export function searchInfoByClass(className, groupId) {
+  return pget(`/pc/operation/user/class/search?className=${className}&&groupId=${groupId}`)
 }
 
-
-export function searchInfoByRiseId(riseId){
-  return pget(`/pc/operation/user/search?type=3&&search=${riseId}`)
-}
