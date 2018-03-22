b994b5f3aef7153fc2fb5e5be84dcc3a3e9a8cb3
xfduan <1047210087@qq.com>
2018-03-22 11:19:34 +0800

    fix

--- src/modules/backend/common/async.tsx
+++ src/modules/backend/common/async.tsx
@@ -1,7 +1,7 @@
 import { ppost } from '../../../utils/request'
 
 export function insertRichText (title, content) {
-  return ppost(`/pc/upload/richText?title=${title}&content=${content}`)
+  return ppost(`/pc/upload/richText`, { title: title, content: content })
 }
 
 export function uploadFile (file) {
